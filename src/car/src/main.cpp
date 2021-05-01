7b663d69b01c6f3901c5a7a9628fabd9143e1f55
Matej Pončák <poncak.matej1@gmail.com>
2021-05-01 20:59:59 +0200

    added car package

---
+++ src/car/src/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+#include <ros/ros.h>
+#include <sensor_msgs/Joy.h>
+
+#include <roboutils/utils.h>
+
+class RosNode
+{
+
+  public:
+    RosNode()
+    {
+      sub = n.subscribe("/joy", 1, &RosNode::callback, this);
+    }
+
+    void callback(const sensor_msgs::Joy& msg)
+    {
+      std::cout << "header: " << std::endl;
+      std::cout << "  seq: " << msg.header.seq << std::endl;
+      std::cout << "  stamp: " << std::endl;
+      std::cout << "    secs: " << msg.header.stamp.sec << std::endl;
+      std::cout << "    nsecs: " << msg.header.stamp.nsec << std::endl;
+      std::cout << "  frame_id: " << msg.header.frame_id << std::endl;
+
+      std::cout << "axes: [";
+      for(int i = 0; i < msg.axes.size(); ++i)
+      {
+        if(i != 0) std::cout << ", ";
+        std::cout << msg.axes[i];
+      }
+      std::cout << "]" << std::endl;
+
+      std::cout << "buttons: [";
+      for(int i = 0; i < msg.buttons.size(); ++i)
+      {
+        if(i != 0) std::cout << ", ";
+        std::cout << msg.buttons[i];
+      }
+      std::cout << "]" << std::endl;
+    }
+
+  private:
+    ros::NodeHandle n;
+    ros::Subscriber sub;
+
+};
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "car");
+
+  RosNode node;
+
+  ros::spin();
+
+  return 0;
+}
